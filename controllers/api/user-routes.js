f5cde793ab681546ae626c5d8f06e429f29ffcad
cat - lin <plummorgan@gmail.com>
2020-11-05 16:33:35 -0800

    made login and logout functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -60,7 +60,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+                
+                res.json(dbUserData);
+            });
+        })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
@@ -97,8 +105,13 @@ router.post('/login', (req, res) => {
             return;
         }
 
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
 
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
     });
 });
 
@@ -119,6 +132,17 @@ router.put('/:id', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
+});
+
+//this is the post request to logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+      } else {
+        res.status(404).end();
+      }
 });
 
 //our DELETE route to api/users/1
